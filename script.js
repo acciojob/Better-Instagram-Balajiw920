acb42e421573814825cf16018e1604e1e04fa0ab
Balajiw920 <wbalaji2021@gmail.com>
2023-01-16 20:22:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+$(document).ready(function() {
+    var parent = document.getElementById("parent");
+
+    // Make all divs with class 'image' draggable
+    $('.image').draggable({
+        helper: 'clone',
+        stop: function(event, ui) {
+        }
+    });
+
+    // Make all divs with class 'image' droppable
+    $('.image').droppable({
+        drop: function(event, ui) {
+            var draggable = ui.draggable;
+            var droppable = $(this);
+            var dragBg = draggable.css('background-image');
+            var dropBg = droppable.css('background-image');
+            draggable.css('background-image', dropBg);
+            droppable.css('background-image', dragBg);
+        }
+    });
+});
